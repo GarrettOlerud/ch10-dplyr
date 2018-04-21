33f97ea1a45d9eededa35857071eecefbf1e805b
Garrett Olerud <golerud701@gmail.com>
2018-04-21 00:11:19 -0700

    11 mins late but had them done for days

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -10,7 +10,9 @@ library(dplyr)
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
-
+largest_arrival_dleay <- flights %>% 
+  group_by(dest) %>% 
+  summarise()
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
